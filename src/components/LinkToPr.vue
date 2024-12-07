03fe4133ea3ef2d9eeea43c0dde009e54202ce6c
Mikhail-2547 <artiprice@gmail.com>
2024-12-07 08:38:40 +0200

    hyperlinking

--- src/components/LinkToPr.vue
+++ src/components/LinkToPr.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="link_to_pr">
-    <a id="home">Home</a>
-    <div>Setting Name</div> <!-- requires adaptivity with pages-->
-    <div>Formulas Name</div> <!-- requires adaptivity with pages-->
+    <div v-on="goHome" id="home">Home</div>
+    <div v-on="goToSetting" >Setting Name</div> <!-- requires adaptivity with pages-->
+    <div v-if="this.$route.name === 'Formula'">Formulas Name</div>
   </div>
 </template>
 <!-- Setting Name and Fomulas Name should be an arguments (name of setting or formula)-->
+
+<script>
+  export default {
+    methods: {
+      goHome() {
+        this.$router.push({ name: 'Home' });
+      },
+      goToSetting() {
+        this.$router.push({ name: 'Setting', params: { id: this.settingId } });
+      },
+    }
+  }
+</script>
